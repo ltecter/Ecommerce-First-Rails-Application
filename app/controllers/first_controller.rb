ca82e9edf570c966335f74c3059a8cd7f6ef211e
Leonard <ltecter@academic.rrc.ca>
2013-03-19 15:36:58 -0500

    A commit message.

--- app/controllers/first_controller.rb
+++ app/controllers/first_controller.rb
@@ -4,4 +4,10 @@ class FirstController < ApplicationController
   end
   #After the index action is called, the associated view
   #will automagically be loaded. app/views/first/index.html.erb
+  
+  def about
+    #Again, for now, this action is empty.
+  end
+  #After the about action is executed, the associated view
+  #will automagically be loaded. app/views/first/about.html.erb
 end
